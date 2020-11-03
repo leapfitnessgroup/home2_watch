bdc245ef892320e527164406bf75d778decfc0d2
xueluwei <xueluwei@deojian.dev>
2020-11-03 19:41:04 +0800

    1.扫码页唤醒问题处理 2.改包名，签名

--- entry/src/main/js/default/pages/qr/qr.js
+++ entry/src/main/js/default/pages/qr/qr.js
@@ -1,9 +1,39 @@
 import app from '@system.app';
+import router from '@system.router';
 
 export default{
+    onInit: function () {
+        this.getMsg();
+    },
+    getMsg: function() {
+        var self = this;
+        FeatureAbility.subscribeMsg({
+            success: function(data) {
+                if(JSON.stringify(data.message) !== undefined){
+                    var json = JSON.parse(data.message);
+                    if ('msg' in json && json.msg === 'exit') {
+                        app.terminate();
+                        console.log('getmsg terminate');
+                    } else {
+                        self.backToIndex();
+                    }
+                }else {
+                    console.log('getmsg undefined');
+                }
+            },
+            fail: function(data, code) {
+                console.log('getmsg fail: ' + code);
+            }
+        });
+    },
     touchMove: function(e){
         if(e.direction === "right"){
             app.terminate();
         }
+    },
+    backToIndex: function () {
+        router.replace({
+            uri: "pages/index/index",
+        });
     }
 }
